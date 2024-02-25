3abb5bdb9ff7df696f8cb2942fb9575d22a48ffa
Maneeshk11 <maneesh.kolli@gmail.com>
2024-02-25 10:55:10 +0530

    projects arrow

--- src/components/contentPage/Projects.tsx
+++ src/components/contentPage/Projects.tsx
@@ -3,6 +3,7 @@ import Tag from "./mini-elements/tag"
 import { Project } from "@/utils/types/interface"
 import projectsList from "@/utils/data/projects.json"
 import ProjectItem from "./mini-elements/ProjectItem"
+import { SlArrowUpCircle, SlArrowDownCircle } from "react-icons/sl";
 
 const Projects = () => {
 
@@ -15,33 +16,39 @@ const Projects = () => {
         } else {
             setProjects(projectsList.filter(item => item.tags.includes(currProjType)))
         }
-        
+
     }, [currProjType])
 
     return (
         <div id="projects" className="flex flex-col items-center gap-y-6 pt-8 w-screen flex-shrink-0">
             <span className="font-semibold text-5xl">My <span className=
-            "font-semibold text-5xl bg-gradient-to-r from-blue-300 to-blue-500 text-transparent bg-clip-text"> projects</span></span>
+                "font-semibold text-5xl bg-gradient-to-r from-blue-300 to-blue-500 text-transparent bg-clip-text"> projects</span></span>
 
             <span className="text-xl">Here is a list of projects I have worked on ...</span>
             <div className="flex flex-row gap-x-2 items-center">
-                <Tag title="All" onClick={() => {setCurrProjType("All")}}/>
-                <Tag title="C++" onClick={() => {setCurrProjType("C++")}}/>
-                <Tag title="Python" onClick={() => {setCurrProjType("Python")}}/>
-                <Tag title="Full-stack" onClick={() => {setCurrProjType("Full-stack")}}/>
-                <Tag title="Crypto" onClick={() => {setCurrProjType("Crypto")}}/>
-                <Tag title="API" onClick={() => {setCurrProjType("API")}}/>
+                <Tag title="All" onClick={() => { setCurrProjType("All") }} />
+                <Tag title="C++" onClick={() => { setCurrProjType("C++") }} />
+                <Tag title="Python" onClick={() => { setCurrProjType("Python") }} />
+                <Tag title="Full-stack" onClick={() => { setCurrProjType("Full-stack") }} />
+                <Tag title="Crypto" onClick={() => { setCurrProjType("Crypto") }} />
+                <Tag title="API" onClick={() => { setCurrProjType("API") }} />
             </div>
 
-            <div className="grid md:grid-cols-2 xl:grid-cols-3 gap-x-6 gap-y-2">
+            <div className="grid md:grid-cols-2 xl:grid-cols-3 gap-x-6 gap-y-2 relative">
                 {
-                    projects && projects.map((item, index)=>{
-                        return(
+                    projects && projects.map((item, index) => {
+                        return (
                             <ProjectItem key={index} details={item} />
-                        )   
+                        )
                     })
                 }
+                <div className="flex flex-col gap-y-6 absolute -right-10 top-[calc(50%-2.5rem)]">
+                    <SlArrowUpCircle className="w-7 h-7 cursor-pointer" />
+                    <SlArrowDownCircle className="w-7 h-7 cursor-pointer" />
+                </div>
             </div>
+
+            {/* projects navigate buttons */}
         </div>
     )
 }
--- src/components/contentPage/mini-elements/ProjectItem.tsx
+++ src/components/contentPage/mini-elements/ProjectItem.tsx
@@ -10,7 +10,7 @@ interface ProjectItemProps {
 const ProjectItem: FC<ProjectItemProps> = ({ details }) => {
     return (
         <div className="w-72 aspect-[10/11] flex items-center justify-center">
-            <div className="w-[17rem] aspect-[10/11] bg-[#1f1f1f] rounded-3xl overflow-hidden cursor-pointer hover:w-full transform duration-100 ease-in"
+            <div className="w-[17rem] aspect-[10/11] bg-[#1d1d1d] rounded-3xl overflow-hidden cursor-pointer hover:w-full transform duration-100 ease-in"
             onClick={() => {window.open(`${details.link}`, '_blank')}}>
                 <div className={`h-2/5 w-full`} style={{ backgroundImage: `url(${details.image})`, backgroundSize: 'cover' }}>
                 </div>
