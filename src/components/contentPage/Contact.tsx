1f5e8d23f86166a05af19babdc7e4d85636086f6
Maneeshk11 <maneesh.kolli@gmail.com>
2024-04-20 07:03:26 +0530

    added toast

--- src/components/contentPage/Contact.tsx
+++ src/components/contentPage/Contact.tsx
@@ -1,11 +1,29 @@
 import Link from "next/link"
 import InputComponent from "../inputcomp"
 import TextArea from "../textarea"
-
+import { useState } from "react"
+import { Bounce, ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Contact = () => {
 
+    const [name, setName] = useState<string>('')
+    const [company, setCompany] = useState<string>('')
+    const [email, setEmail] = useState<string>('')
+    const [message, setMessage] = useState<string>('')
 
+    const sendMessage = () => toast.success('Message Sent Sucessfully', {
+        className: "toast-position",
+        position: "top-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+        transition: Bounce,
+        });
 
     return (
         <div id="contact" className="w-screen flex flex-col items-center flex-shrink-0 transform duration-300 ease-in gap-y-8 mb-auto mt-24">
@@ -23,9 +41,24 @@ const Contact = () => {
             <Link
                 className="inline-block rounded bg-indigo-700 px-8 py-2 text-sm font-medium text-white transition hover:rotate-2 hover:scale-110 focus:outline-none focus:ring active:bg-indigo-500"
                 href="#"
+                onClick={sendMessage}
             >
-                Send 
+                Send
             </Link>
+
+            <ToastContainer
+                position="top-center"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="colored"
+                transition={Bounce}
+            />
         </div>
     )
 }
--- src/components/textarea.tsx
+++ src/components/textarea.tsx
@@ -1,22 +1,32 @@
+import { useState } from "react"
 
 const TextArea = () => {
+
+    const [message, setMessage] = useState<string>('')
+
+    const ClearText = () => {
+        setMessage('')
+    }
+
     return (
         <div>
             <div
                 className="overflow-hidden rounded-lg bg-[#1d1d1d] shadow-sm focus-within:ring-1 focus-within:ring-slate-600 w-72"
             >
                 <textarea
-                    id="OrderNotes"
+                    id="messageBox"
                     className="w-full resize-none border-none align-top focus:ring-0 sm:text-sm bg-[#1d1d1d] dark:text-white placeholder:text-gray-300"
                     rows={4}
                     placeholder="Type message here ..."
+                    value={message}
+                    onChange={(e) => setMessage(e.target.value)}
                 ></textarea>
 
                 <div className="flex items-center justify-end gap-2 p-3 bg-[#1d1d1d] w-full">
-                    
                     <button
                         type="button"
                         className="rounded bg-indigo-700 px-3 py-1.5 text-sm font-medium text-white hover:bg-indigo-800"
+                        onClick={ClearText}
                     >
                         Clear
                     </button>
