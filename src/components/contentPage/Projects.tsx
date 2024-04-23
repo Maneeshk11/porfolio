085c763f00eb96e21b7b287a0df2ae07e9334759
Maneeshk11 <maneesh.kolli@gmail.com>
2024-04-23 13:38:53 +0530

    animated scroll bar

--- src/components/contentPage/Projects.tsx
+++ src/components/contentPage/Projects.tsx
@@ -1,13 +1,23 @@
-import { useEffect, useRef, useState } from "react"
+import { useEffect, useState, useRef } from "react"
 import Tag from "./mini-elements/tag"
 import { Project } from "@/utils/types/interface"
 import projectsList from "@/utils/data/projects.json"
 import ProjectItem from "./mini-elements/ProjectItem"
+import { useScroll, motion, useTransform } from "framer-motion"
 
 const Projects = () => {
 
     const [currProjType, setCurrProjType] = useState<string>("All")
     const [projects, setProjects] = useState<Project[]>([])
+
+    const projectsRef = useRef<HTMLDivElement>(null)
+
+    const { scrollYProgress } = useScroll({
+        container: projectsRef
+    })
+
+    const pos = useTransform(scrollYProgress, [0, 1], [0, 40])
+
 
     useEffect(() => {
         if (currProjType === "All") {
@@ -17,7 +27,7 @@ const Projects = () => {
         }
 
     }, [currProjType])
-    
+
 
     return (
         <div id="projects" className="flex flex-col items-center gap-y-6 pt-8 w-screen flex-shrink-0">
@@ -35,7 +45,7 @@ const Projects = () => {
             </div>
 
             <div className="px-8 relative">
-                <div className="projectsDiv h-[40rem] grid md:grid-cols-2 xl:grid-cols-3 gap-x-6 gap-y-2 overflow-y-scroll scroll">
+                <div ref={projectsRef} className="projectsDiv h-[40rem] grid md:grid-cols-2 xl:grid-cols-3 gap-x-6 gap-y-2 overflow-y-scroll scroll">
                     {
                         projects && projects.map((item, index) => {
                             return (
@@ -45,7 +55,10 @@ const Projects = () => {
                     }
                 </div>
                 <div className="absolute top-[calc(50%-2rem)] right-1 rounded-full w-2 h-16 bg-[#2e2d2d]">
-                    <div className="scroll w-full rounded-full h-6 bg-white"></div>
+                    <div className="w-full h-full relative">
+                        <motion.div className="absolute scroll w-full rounded-full h-6 bg-white"
+                            style={{ top: pos }}></motion.div>
+                    </div>
                 </div>
 
             </div>
