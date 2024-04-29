67008f396da788b467c8bc1c054185b39dd14c70
Maneeshk11 <maneesh.kolli@gmail.com>
2024-04-29 06:43:22 +0530

    width notbeing set to problem type div

--- src/components/blogComponents/LeetcodeItem.tsx
+++ src/components/blogComponents/LeetcodeItem.tsx
@@ -12,14 +12,14 @@ interface LeetcodeItemProps {
 const LeetcodeItem: FC<LeetcodeItemProps> = ({ title, problemType, setIsOpen, setTitle, setFilename, filename }) => {
 
     return (
-        <div className="h-10 w-36 sm:w-40 md:w-56 text-sm bg-[#1d1d1d] rounded overflow-hidden p-2 flex items-center cursor-pointer gap-x-3 text-nowrap"
+        <div className="h-10 w-36 sm:w-40 md:w-56 text-sm bg-[#1d1d1d] rounded overflow-hidden p-2 flex items-center cursor-pointer gap-x-3"
             onClick={() => { 
                 setIsOpen(true);
                 setTitle(title);
                 setFilename(filename);
             }}>
-            <div className={`w-4 aspect-square rounded-full ${problemType.toLowerCase() === "easy" ? "bg-[#00b8a3]" : problemType.toLowerCase()  === "medium" ? "bg-[#ffc01e]" : "bg-[#fe365e]"} `}></div>
-            <span>{title}</span>
+            <div className={`min-w-4 aspect-square rounded-full ${problemType.toLowerCase() === "easy" ? "bg-[#00b8a3]" : problemType.toLowerCase()  === "medium" ? "bg-[#ffc01e]" : "bg-[#fe365e]"} `}></div>
+            <span className=" text-nowrap">{title}</span>
         </div>
     )
 }
