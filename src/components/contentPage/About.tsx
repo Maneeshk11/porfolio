ffdfc79a6381091c9abfb03ce7752ab0c6682cd8
Maneeshk11 <maneesh.kolli@gmail.com>
2024-05-05 13:21:48 +0530

    animate drop in

--- src/components/contentPage/About.tsx
+++ src/components/contentPage/About.tsx
@@ -4,7 +4,7 @@ import Timeline from "./timeline/Timeline"
 const About = () => {
     return (
         <div id="about" className="flex flex-col items-center gap-y-6 w-screen flex-shrink-0 mb-auto mt-24">
-            <div className="mb-48 flex flex-col items-center gap-y-6 ">
+            <div className="mb-48 flex flex-col items-center gap-y-6 animate-drop-in">
                 <span className="font-semibold text-5xl">Hi, I&apos;m <span className=
                     "font-semibold text-5xl bg-gradient-to-r from-[#ffcbf2] to-[#deaaff] text-transparent bg-clip-text"> Maneesh</span></span>
 
